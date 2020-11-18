d46368018aa3005ec97a7a82f8ac42aa42cf9d19
jmseo <wjdals5190@naver.com>
2020-11-18 21:39:59 +0900

    PushPrint.java modified on Local repository

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -1,6 +1,7 @@
 
 public class PushPrint {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("sophomore");
+		System.out.println("Wankwang Univ");
 	}
 }
